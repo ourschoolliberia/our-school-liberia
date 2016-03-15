7d98b94233add2454208ef9556ff0ab7137fab6c
Andrea Lizard <a.agathocleous92@gmail.com>
2016-03-15 21:11:30 +1100

    front page language

--- locales/de.js
+++ locales/de.js
@@ -1,6 +1,17 @@
 {
-	"backto": "backto",
-	"updates": "updates",
-
-	"wherewework-title": "Vhere Ve Verk"
+	"backto": "Back To",
+	"updates": "Updates",
+	"intro": {
+		"donatenow": "SPENDE JETZT",
+		"aboutourschool": "UNSERE SCHULE",
+		"aboutourschoolprev": "Unsere Schule wurde im Januar 2006 in Monrovia , der Hauptstadt von Liberia gegründet. Wir haben es uns zum Ziel gesetzt, eine Schule für benachteiligte Kinder zu bauen, die finanziellen Mittel fehlen . Wir wollen eine Ausbildung zu bekommen und die Möglichkeit, diese Kinder trotz dieser schwierigen Situation bieten ...",
+		"readmore": "WEITERLESEN"
+	},
+	"about": {
+		"wherewework": {
+			"wherewework-title": "wo-wir-arbeiten",
+			"maincontent": "Dies ist der Hauptinhalt"
+		}
+	},
+	"wherewework-title": "wherewework-title"
 }
--- locales/en.js
+++ locales/en.js
@@ -1,9 +1,15 @@
 {
 	"backto": "Back To",
 	"updates": "Updates",
-	"wherewework-title": "Where We Work",
+	"intro": {
+		"donatenow": "DONATE NOW",
+		"aboutourschool": "ABOUT OUR SCHOOL",
+		"aboutourschoolprev": "Our school was founded in January 2006 in Monrovia, capital of Liberia. We have made it our goal to build a school for disadvantaged children, who lack financial resources. We want to get an education and provide an opportunity to these children despite this difficult situation...",
+		"readmore": "READ MORE"
+	},
 	"about": {
 		"wherewework": {
+			"wherewework-title": "Where We Work",
 			"maincontent": "This is the maincontent"
 		}
 	}
