4b08c69464a0c06998645079c3fcd8a6b8bc6b3b
Andrea Lizard <a.agathocleous92@gmail.com>
2016-05-23 07:45:32 +1000

    trying to get team members to display as images with captions

--- public/js/about-ourteam.js
+++ public/js/about-ourteam.js
@@ -1,12 +1,5 @@
 $( document ).ready(function() {
-	$('.ourteam').slick({
-		infinite: true,
-		slidesToShow: 2,
-		slidesToScroll: 1
-	});
-	$('.ourteam-small').slick({ 
-		infinite: true,
-	 	slidesToShow: 1,
-		slidesToScroll: 1
+	$('.gallery').poptrox({
+		usePopupCaption: true
 	});
 });
