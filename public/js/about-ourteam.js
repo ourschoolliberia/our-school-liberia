7a237c394b219948439fab8dc07e43c10f1e789d
Andrea Lizard <a.agathocleous92@gmail.com>
2016-05-28 21:52:23 +1000

    thumbnail gallery for teams

--- public/js/about-ourteam.js
+++ public/js/about-ourteam.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
 	$('.gallery').poptrox({
-		usePopupCaption: true
+		preload: true,
+		usePopupCaption: true,
+		caption: { selector: "section", remove: true }
 	});
 });
