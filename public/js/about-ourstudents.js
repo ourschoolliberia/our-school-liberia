ee2e9ce3e9d8ab38a09fed0521d2d387fc523b2a
Andrea Agathocleous <andrea.agathocleous@bigmobile.com>
2016-06-04 22:55:02 +1000

    Students update

--- public/js/about-ourstudents.js
+++ public/js/about-ourstudents.js
@@ -1,14 +1,7 @@
 $( document ).ready(function() {
-	$('.ourstudents').slick({
-		infinite: true,
-		slidesToShow: 2,
-		speed: 100,
-		slidesToScroll: 1
-	});
-	$('.ourstudents-small').slick({ 
-		infinite: true,
-	 	slidesToShow: 1,
-	 	speed: 100,
-		slidesToScroll: 1
+	$('.gallery').poptrox({
+		preload: true,
+		usePopupCaption: true,
+		caption: { selector: "section", remove: true }
 	});
 });
